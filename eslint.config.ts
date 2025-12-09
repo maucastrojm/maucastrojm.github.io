9c635cad31dd56754d51f6b05375060e01e1db09
Melody Castro <mau.castrojm@gmail.com>
2025-12-09 16:44:30 -0300

    Boilerplate and ESLint

---
+++ eslint.config.ts
@@ -0,0 +1,56 @@
+import { globalIgnores } from 'eslint/config';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
+import pluginVitest from '@vitest/eslint-plugin';
+import eslintPrettier from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  ...tseslint.configs.recommended,
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommended,
+  eslintPrettier,
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    rules: {
+      'vue/padding-line-between-blocks': ['off'],
+      'vue/max-attributes-per-line': ['off'],
+      'vue/first-attribute-linebreak': [
+        'error',
+        {
+          singleline: 'ignore',
+          multiline: 'below',
+        },
+      ],
+      'vue/multi-word-component-names': ['off'],
+      'vue/component-definition-name-casing': ['error', 'PascalCase'],
+      'vue/prop-name-casing': ['error', 'camelCase'],
+      'vue/component-name-in-template-casing': ['error', 'PascalCase', { registeredComponentsOnly: false }],
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'no-console': ['error'],
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: {
+            void: 'always',
+            normal: 'always',
+            component: 'always',
+          },
+          svg: 'always',
+          math: 'always',
+        },
+      ],
+      'vue/require-default-prop': ['off'],
+    },
+  },
+);
---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath } from 'node:url';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'happy-dom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html-spa'],
+        exclude: ['node_modules', 'patches', 'public', '__mocks__', 'app/assets'],
+      },
+    },
+  }),
+);
